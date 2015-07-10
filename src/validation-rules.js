a8e95aef06d8fe91e1b1903b1e7a9f5e075441e8
Diego Pessoa <diegopessoa12@gmail.com>
2015-07-10 17:49:50 -0300

    Changing regex for e-mail validation
    The previous regex was accepting values with dots in the end and in the
    beginning. It was changed to the w3c recommendation:
    http://www.w3.org/TR/html5/forms.html#valid-e-mail-address.

--- src/validation-rules.js
+++ src/validation-rules.js
@@ -389,9 +389,8 @@ angular
           break;
         case "email" :
           validator = {
-            // Email RFC 5322, pattern pulled from  http://www.regular-expressions.info/email.html
-            // but removed necessity of a TLD (Top Level Domain) which makes this email valid: admin@mailserver1
-            pattern: "(?:[\\wа-я0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.?[\\wа-я0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[\\wа-я0-9](?:[\\wа-я0-9-]*[\\wа-я0-9])?\\.?)+[\\wа-я0-9](?:[\\wа-я0-9-]*[\\wа-я0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.?){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[\\wа-я0-9-]*[\\wа-я0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])",
+            // Email pattern pulled from http://www.w3.org/TR/html5/forms.html#valid-e-mail-address
+            pattern: "/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/",
             message: "INVALID_EMAIL",
             type: "regex"
           };
